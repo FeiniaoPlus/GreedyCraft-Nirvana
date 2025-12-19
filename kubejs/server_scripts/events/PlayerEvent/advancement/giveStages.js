ef51b2ac3b6752f4ba13d58bdb7fbe6d8844927e
create-xiaoyu <abc18873060306@qq.com>
2025-12-19 14:18:03 +0800

    监听完成进度给予阶段

---
+++ kubejs/server_scripts/events/PlayerEvent/advancement/giveStages.js
@@ -0,0 +1,14 @@
+PlayerEvents.advancement("greedycraft:init", event => {
+    let player = event.player
+    AStages.addStageToPlayer("init", player)
+})
+
+PlayerEvents.advancement("greedycraft:stages/init_start", event => {
+    let player = event.player
+    AStages.addStageToPlayer("init_start", player)
+})
+
+PlayerEvents.advancement("greedycraft:stages/init_creative", event => {
+    let player = event.player
+    AStages.addStageToPlayer("init_creative", player)
+})
