13c4cf62f895a7ccc25454e32193d7aebe78252c
create-xiaoyu <abc18873060306@qq.com>
2025-12-19 14:18:52 +0800

    添加初始阶段

---
+++ kubejs/startup_scripts/registry/stages.js
@@ -0,0 +1,3 @@
+AStages.addRestrictionForItem("greedycraft_stage_init", "init", 'greedycraft:placeholder')
+AStages.addRestrictionForItem("greedycraft_stage_init_start", "init_start", 'greedycraft:placeholder')
+AStages.addRestrictionForItem("greedycraft_stage_init_creative", "init_creative", 'greedycraft:placeholder')
