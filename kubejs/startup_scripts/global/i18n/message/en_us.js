46f51031e97bf44e694f7835927048ad0da35137
create-xiaoyu <abc18873060306@qq.com>
2025-12-23 12:49:57 +0800

    添加本地化文件

---
+++ kubejs/startup_scripts/global/i18n/message/en_us.js
@@ -0,0 +1,46 @@
+// priority: 600
+
+global.en_us_Message = [
+  {
+    text: "§eExperiencing lag? When memory usage is high, use the §6Free Memory§e button in the §6R§e key menu to manually clear memory!"
+  },
+  {
+    text: "§eThanks for playing GreedyCraft! If you encounter any issues, please refer to the §6'GreedyCraft – From Beginner to Master'§e guide first."
+  },
+  {
+    text: "§eIf you encounter mod-related issues, please check the §6Encyclopedia§e! Guides for all mods can be found there."
+  },
+  {
+    text: "§eThis modpack includes a vast amount of music and improved sound effects. Strongly recommend playing with headphones!"
+  },
+  {
+    text: "§eYou can switch game modes in the Quest Menu! The Quest Menu can be found in the top-left corner of your inventory screen."
+  },
+  {
+    text: "§eYou can adjust the game mode before entering the world! The configuration file is located at §6config/greedycraft/config.json§e. Modify the content after §6packmode=§e to change the game mode!"
+  },
+  {
+    text: "§eYou can use §6/setPackModeTo <mode>§e to switch the game mode."
+  },
+  {
+    text: "§eIf you experience background music stopping for long periods, you can press §6Alt + Numpad -§e to reload the dynamic music!"
+  },
+  {
+    text: "§eIf you experience lag, try pressing §6R§e to open the menu and click the '§6Free Memory§e' button. Alternatively, try adjusting your render distance down and then back up."
+  },
+  {
+    text: "§eYou can use §6/hidescoreboard§e to hide the scoreboard on the right! Use §6/showscoreboard§e to show it again."
+  },
+  {
+    text: "§eYou can use §6/hidedeathquotes§e to disable death message displays, or §6/showdeathquotes§e to re-enable them!"
+  },
+  {
+    text: "§eYou can use §6/backup§e to manually back up your world. You can find the option to restore backups on the world selection screen!"
+  },
+  {
+    text: "§ePress §6B§e to create a new waypoint at your location. Press §6B§e on the map screen to create one at the cursor position!"
+  },
+  {
+    text: "§eWhen updating the modpack version, move the §6saves§e folder from the old installation's root directory to the same location in the new version to preserve your worlds!"
+  }
+]
