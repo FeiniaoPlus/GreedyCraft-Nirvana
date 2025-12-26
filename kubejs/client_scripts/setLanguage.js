3c49454d3fff00538e1fe7b003e58f9e7d6d3450
create-xiaoyu <abc18873060306@qq.com>
2025-12-26 14:09:27 +0800

    添加日志

--- kubejs/client_scripts/setLanguage.js
+++ kubejs/client_scripts/setLanguage.js
@@ -2,9 +2,12 @@ let aotoSetLanguage = global.aotoSetLanguage
 
 if (aotoSetLanguage == "true") {
     let language = KJSutils.GetSystemLanguage()
+    LOGGER("info", `System Language: ${language}`)
     if (language == "en-us") {
         KJSutils.ModifyJsonValue("config/greedycraft/config.json", "$.language", "en_us")
+        LOGGER("info", `Change Language to en_us`)
     } else if (language == "zh-cn") {
         KJSutils.ModifyJsonValue("config/greedycraft/config.json", "$.language", "zh_cn")
+        LOGGER("info", `Change Language to zh_cn`)
     }
 }
