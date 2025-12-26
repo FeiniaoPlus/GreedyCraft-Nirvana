fd3b86531c392cf2aa6a7c6b44f30e0837505d82
create-xiaoyu <abc18873060306@qq.com>
2025-12-26 16:51:33 +0800

    添加反作弊系统状态消息

--- kubejs/server_scripts/events/PlayerEvent/loggedIn/seedMessage.js
+++ kubejs/server_scripts/events/PlayerEvent/loggedIn/seedMessage.js
@@ -4,6 +4,10 @@ PlayerEvents.loggedIn(event => {
     let player = event.player
     let playerName = event.player.username
     let cheat = checkCheat(player)
+    
+    let antiCheatMode = KJSutils.Analysis("config/greedycraft/config.json", "$.antiCheatMode")
+    let antiCheat = KJSutils.Analysis("config/greedycraft/config.json", "$.antiCheat")
+
     let packLanguage = KJSutils.Analysis("config/greedycraft/config.json", "$.language")
     let messageTitle = KJSutils.Analysis(`kubejs/assets/greedycraft/lang/${packLanguage}.json`, "$$.greedycraft.message.playerlogging.system.title")
     let messageText1 = KJSutils.Analysis(`kubejs/assets/greedycraft/lang/${packLanguage}.json`, "$$.greedycraft.message.playerlogging.system.text.1") + `§6${playerName}`
@@ -16,6 +20,7 @@ PlayerEvents.loggedIn(event => {
     let messageText8 = KJSutils.Analysis(`kubejs/assets/greedycraft/lang/${packLanguage}.json`, "$$.greedycraft.message.playerlogging.system.text.8")
     let messageText9 = KJSutils.Analysis(`kubejs/assets/greedycraft/lang/${packLanguage}.json`, "$$.greedycraft.message.playerlogging.system.text.9")
     let messageEnd = KJSutils.Analysis(`kubejs/assets/greedycraft/lang/${packLanguage}.json`, "$$.greedycraft.message.playerlogging.system.text.end")
+
     let message
     let unofficialModList = checkModList()
     if (!(AStages.playerHasStage("init", player))) {
@@ -29,6 +34,13 @@ PlayerEvents.loggedIn(event => {
         player.tell(messageText7)
         player.tell(messageText8)
         player.tell(messageText9)
+        if (antiCheat) {
+            let message = KJSutils.Analysis(`kubejs/assets/greedycraft/lang/${packLanguage}.json`, "$$.greedycraft.message.anticheat.runing.text")
+            player.tell(`${message} + ${antiCheatMode}`)
+        } else {
+            let message = KJSutils.Analysis(`kubejs/assets/greedycraft/lang/${packLanguage}.json`, "$$.greedycraft.message.anticheat.off.text")
+            player.tell(`${message}`)
+        }
         player.tell(messageEnd)
     } else {
         switch (packLanguage) {
@@ -46,7 +58,7 @@ PlayerEvents.loggedIn(event => {
     }
 
     if (cheat) {
-        if (unofficialModList.length > 0) {
+        if (unofficialModList.length >= 0) {
             let message = KJSutils.Analysis(`kubejs/assets/greedycraft/lang/${packLanguage}.json`, "$$.greedycraft.message.cheat.modlist.text")
             player.tell(message)
             for (mods in unofficialModList) {
