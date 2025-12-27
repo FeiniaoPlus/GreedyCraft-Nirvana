5ceac7016f450f584ef8af4b8f3b186e80654a1c
create-xiaoyu <abc18873060306@qq.com>
2025-12-27 12:17:05 +0800

    添加注释，修复非官方模组列表判断

--- kubejs/server_scripts/utils/antiCheat.js
+++ kubejs/server_scripts/utils/antiCheat.js
@@ -1,5 +1,6 @@
 // priority: 500
 
+// 函数：检查是否有非官方模组，返回非官方模组列表
 function checkModList() {
     let unofficialModList = []
 
@@ -12,13 +13,15 @@ function checkModList() {
     return unofficialModList
 }
 
+// 函数：检查是否作弊，返回布尔值。要求提供 event.player 对象
 function checkCheat(player) {
     let cheat = false
     let unofficialModList = checkModList()
-    if (unofficialModList.length >= 0) {
+    if (unofficialModList.length > 0) {
         cheat = true
     }
     if (player.isCreative()) {
+        // 判断玩家在创造模式下有没有 init_creative 阶段
         if (!AStages.playerHasStage("init_creative", player)) {
             cheat = true
         }
