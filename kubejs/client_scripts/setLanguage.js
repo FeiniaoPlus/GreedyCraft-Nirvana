d792fb5d64b957ad5b5638224c47a08d62f95759
create-xiaoyu <abc18873060306@qq.com>
2025-12-31 01:15:08 +0800

    将if改为switch

--- kubejs/client_scripts/setLanguage.js
+++ kubejs/client_scripts/setLanguage.js
@@ -1,15 +1,21 @@
 // 根据系统语言设置整合包语言
 
-let aotoSetLanguage = global.aotoSetLanguage
+let autoSetLanguage = global.autoSetLanguage
 
-if (aotoSetLanguage == "true") {
+if (autoSetLanguage == "true") {
     let language = KJSutils.GetSystemLanguage()
     LOGGER("info", `System Language: ${language}`)
-    if (language == "en-us") {
-        KJSutils.ModifyJsonValue("config/greedycraft/config.json", "$.language", "en_us")
-        LOGGER("info", `Change Language to en_us`)
-    } else if (language == "zh-cn") {
-        KJSutils.ModifyJsonValue("config/greedycraft/config.json", "$.language", "zh_cn")
-        LOGGER("info", `Change Language to zh_cn`)
+    switch (language) {
+        case "en-US":
+            KJSutils.ModifyJsonValue("config/greedycraft/config.json", "$.language", "en_us")
+            LOGGER("info", `Change Language to en_us`)
+            break
+        case "zh-CN":
+            KJSutils.ModifyJsonValue("config/greedycraft/config.json", "$.language", "zh_cn")
+            LOGGER("info", `Change Language to zh_cn`)
+            break
+        default:
+            KJSutils.ModifyJsonValue("config/greedycraft/config.json", "$.language", "en_us")
+            LOGGER("info", `Change Language to en_us`)
     }
 }
