55b3ad2b28c4e3e7a5c26504f4cca9018792703f
create-xiaoyu <abc18873060306@qq.com>
2025-12-31 18:25:57 +0800

    修改基础阶段注册

---
+++ kubejs/server_scripts/astages/RestrictionStageItem.js
@@ -0,0 +1,5 @@
+// priority: 250
+
+AStages.addRestrictionForItem("greedycraft_stage/item/init", "init", ['greedycraft:placeholder'])
+AStages.addRestrictionForItem("greedycraft_stage/item/init", "init_start", ['greedycraft:placeholder'])
+AStages.addRestrictionForItem("greedycraft_stage/item/init", "init_creative", ['greedycraft:placeholder'])
--- kubejs/server_scripts/astages/restrictionStage.js
+++
@@ -1,6 +0,0 @@
-// priority: 50
-
-//注册astages阶段
-AStages.addRestrictionForItem("greedycraft_stage_init", "init", 'greedycraft:placeholder')
-AStages.addRestrictionForItem("greedycraft_stage_init_start", "init_start", 'greedycraft:placeholder')
-AStages.addRestrictionForItem("greedycraft_stage_init_creative", "init_creative", 'greedycraft:placeholder')
