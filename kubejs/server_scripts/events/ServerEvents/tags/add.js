030b1a6f10f345626b2978955d14deefdebe0a5f
create-xiaoyu <abc18873060306@qq.com>
2026-01-09 16:56:15 +0800

    为某些物品添加tag

--- kubejs/server_scripts/events/ServerEvents/tags/add.js
+++ kubejs/server_scripts/events/ServerEvents/tags/add.js
@@ -11,6 +11,18 @@ ServerEvents.tags("item", event => {
         'avaritia:end_crafting_table',
         'avaritia:extreme_crafting_table'
     ])
+    Item.getList().forEach(item => {
+        if (item.id.match(/^rechiseled.*planks/)) {
+            event.add("c:blocks/planks", item.id)
+        }
+        if (item.id.match(/.*quartz_block.*/)) {
+            event.add("c:blocks/quartz_block", item.id)
+        }
+    })
+    event.add("c:blocks/planks", [
+        'defiled_lands_reborn:tenebra_planks',
+        'twilightforest:encased_towerwood'
+    ])
 })
 
 ServerEvents.tags("block", event => {
