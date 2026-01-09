d77a5624f378e116b3ffa37c70a95077ab1acb94
create-xiaoyu <abc18873060306@qq.com>
2026-01-09 16:53:24 +0800

    注册新物品

--- kubejs/startup_scripts/registry/item/normal.js
+++ kubejs/startup_scripts/registry/item/normal.js
@@ -4,4 +4,12 @@
 StartupEvents.registry("item", event => {
     event.create("greedycraft:logo")
     event.create("greedycraft:placeholder")
+    event.create("greedycraft:twilight_gem")
+        .glow(true)
+        .tooltip(Component.translatable("greedycraft.item.twilight_gem.tooltip.1.text"))
+        .tooltip(Component.translatable("greedycraft.item.twilight_gem.tooltip.2.text"))
+    event.create("greedycraft:twilight_shield")
+        .glow(true)
+        .tooltip(Component.translatable("greedycraft.item.twilight_shield.tooltip.1.text"))
+        .tooltip(Component.translatable("greedycraft.item.twilight_shield.tooltip.2.text"))
 })
