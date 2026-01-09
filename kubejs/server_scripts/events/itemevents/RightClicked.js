73153595525424ab217382aa5d4b2b84994d7145
create-xiaoyu <abc18873060306@qq.com>
2026-01-09 16:55:48 +0800

    添加暮色之蔽护物品右键事件

---
+++ kubejs/server_scripts/events/itemevents/RightClicked.js
@@ -0,0 +1,10 @@
+ItemEvents.rightClicked("greedycraft:twilight_shield", event => {
+    let player = event.player
+    let playerName = player.username
+    let server = event.server
+
+    event.item.shrink(1)
+    server.runCommandSilent(`advancement grant ${playerName} only greedycraft:twilight_shield`)
+    server.runCommandSilent(`execute at ${playerName} run particle minecraft:totem_of_undying ~ ~ ~ 2 2 2 5 1000 force`)
+    console.log(`Give ${playerName} for greedycraft:stages/twilight_shield Advancement`)
+})
