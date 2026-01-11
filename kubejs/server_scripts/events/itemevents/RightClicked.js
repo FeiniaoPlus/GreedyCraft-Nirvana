be4f389d1eddd195c6207b12ae256283c05fe1a4
create-xiaoyu <abc18873060306@qq.com>
2026-01-11 16:23:16 +0800

    添加末影护符右键事件

--- kubejs/server_scripts/events/itemevents/RightClicked.js
+++ kubejs/server_scripts/events/itemevents/RightClicked.js
@@ -9,7 +9,23 @@ ItemEvents.rightClicked("greedycraft:twilight_shield", event => {
     event.item.shrink(1)
 
     // 给予暮色庇护进度
-    server.runCommandSilent(`advancement grant ${playerName} only greedycraft:twilight_shield`)
+    server.runCommandSilent(`advancement grant ${playerName} only greedycraft:stages/twilight_shield`)
+
+    // 生成粒子
+    server.runCommandSilent(`execute at ${playerName} run particle minecraft:totem_of_undying ~ ~ ~ 2 2 2 5 1000 force`)
+    console.log(`Give ${playerName} for greedycraft:stages/twilight_shield Advancement`)
+})
+
+ItemEvents.rightClicked("greedycraft:ender_charm", event => {
+    let player = event.player
+    let playerName = player.username
+    let server = event.server
+
+    // 将物品减1
+    event.item.shrink(1)
+
+    // 给予终末之路进度
+    server.runCommandSilent(`advancement grant ${playerName} only greedycraft:stages/ender_charm`)
 
     // 生成粒子
     server.runCommandSilent(`execute at ${playerName} run particle minecraft:totem_of_undying ~ ~ ~ 2 2 2 5 1000 force`)
