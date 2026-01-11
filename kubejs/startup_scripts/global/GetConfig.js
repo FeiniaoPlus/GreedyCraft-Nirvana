50755f64a7f558afabed6f1892014d0cb5c6ee6b
create-xiaoyu <abc18873060306@qq.com>
2026-01-11 16:40:23 +0800

    修复自动设置游戏语言

--- kubejs/startup_scripts/global/GetConfig.js
+++ kubejs/startup_scripts/global/GetConfig.js
@@ -2,7 +2,7 @@
 
 // 获取整合包基础配置
 global.packLanguage = KJSutils.Analysis("config/greedycraft/config.json", "$.language")
-global.aotoSetLanguage = KJSutils.Analysis("config/greedycraft/config.json", "$.aotoSetLanguage")
+global.autoSetLanguage = KJSutils.Analysis("config/greedycraft/config.json", "$.autoSetLanguage")
 global.packMode = KJSutils.Analysis("config/greedycraft/config.json", "$.packMode")
 global.checkVersionUpdate = KJSutils.Analysis("config/greedycraft/config.json", "$.checkVersionUpdate")
 global.antiCheat = KJSutils.Analysis("config/greedycraft/config.json", "$.antiCheat")
