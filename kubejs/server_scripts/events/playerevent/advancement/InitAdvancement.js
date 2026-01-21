8251b2883fd74eda943d8eb5c2d58f85ce5c7d7f
create-xiaoyu <abc18873060306@qq.com>
2026-01-21 21:49:04 +0800

    将基础进度给予拆分出来

---
+++ kubejs/server_scripts/events/playerevent/advancement/InitAdvancement.js
@@ -0,0 +1,15 @@
+// 根据游戏特征给予对应进度
+
+PlayerEvents.advancement("greedycraft:init", event => {
+    let player = event.player
+    let playerName = player.username
+    // 如果玩家没有 init_creative、init_start 阶段并且处于创造模式则是以创造模式创建的存档，给予玩家 init_creative 进度
+    if (!(AStages.playerHasStage("init_creative", player)) && !(AStages.playerHasStage("init_start", player)) && player.isCreative()) {
+        event.server.runCommandSilent(`advancement grant ${playerName} only greedycraft:stages/init_creative`)
+        console.log(`Give ${playerName} for greedycraft:stages/init_creative Advancement`)
+    } else {
+        // 否则是以生存或冒险模式创建的存档，给予 init_start 进度
+        event.server.runCommandSilent(`advancement grant ${playerName} only greedycraft:stages/init_start`)
+        console.log(`Give ${playerName} for greedycraft:stages/init_start Advancement`)
+    }
+})
