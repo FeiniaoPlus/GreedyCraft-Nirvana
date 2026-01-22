1e4e0624918cb9fecb6c68f9bc684c9dbafacd68
create-xiaoyu <abc18873060306@qq.com>
2026-01-22 22:12:27 +0800

    休闲模式下无阶段相关操作

--- kubejs/server_scripts/events/playerevent/advancement/GiveStages.js
+++ kubejs/server_scripts/events/playerevent/advancement/GiveStages.js
@@ -5,18 +5,22 @@ PlayerEvents.advancement(event => {
     let advancementNamespace = event.advancement.id.getNamespace()
     let advancementPath = event.advancement.id.getPath().toString()
 
-    // 判断进度的命名空间是否是greedycraft
-    if (advancementNamespace == "greedycraft") {
-        // 判断进度路径开头是否是stages/
-        if (advancementPath.startsWith("stages/")) {
-            // 截断多余字符
-            let stage = advancementPath.replace("stages/", "")
-            // 判断该阶段是否已解锁
-            if (!(AStages.playerHasStage(stage, player))) {
-                // 给予玩家进度
-                AStages.addStageToPlayer(stage, player)
-                // 播放声音
-                player.playNotifySound("minecraft:ui.toast.challenge_complete", "music", 1.0, 1.0)
+    let packMode = KJSutils.Analysis("config/greedycraft/config.json", "$.packMode")
+
+    if (!(packMode == "casual")) {
+        // 判断进度的命名空间是否是greedycraft
+        if (advancementNamespace == "greedycraft") {
+            // 判断进度路径开头是否是stages/
+            if (advancementPath.startsWith("stages/")) {
+                // 截断多余字符
+                let stage = advancementPath.replace("stages/", "")
+                // 判断该阶段是否已解锁
+                if (!(AStages.playerHasStage(stage, player))) {
+                    // 给予玩家进度
+                    AStages.addStageToPlayer(stage, player)
+                    // 播放声音
+                    player.playNotifySound("minecraft:ui.toast.challenge_complete", "music", 1.0, 1.0)
+                }
             }
         }
     }
